ed9e17210dc5c5d9333093d61a347e1d2da7cff3
kali <kali@kali.kali>
2025-04-09 16:44:00 -0600

    <Edition> API URls edition to noly one

--- api_app/urls.py
+++ api_app/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'mangas', views.MangaViewSet, basename = 'mangas')
-router_artist = routers.DefaultRouter()
-router_artist.register(r'artists', views.ArtistViewSet, basename = 'artists')
-
+router.register(r'artists', views.ArtistViewSet, basename= 'artists')
 urlpatterns = [
     path('api/mangas_library/', include(router.urls)),
-    path('api/artists/', include(router_artist.urls)),
 ]
